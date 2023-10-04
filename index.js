c68013feb624db051afe5d119feb7ff5a7716f5e
ubaid ghori <ubaid.razaghori@gmail.com>
2023-10-04 09:28:12 +0500

    go

---
+++ index.js
@@ -0,0 +1,11 @@
+function generateGradient() {
+    const color1 = document.getElementById("color1").value;
+    const color2 = document.getElementById("color2").value;
+    const gradientBox = document.getElementById("gradient-box");
+
+    gradientBox.style.background = `linear-gradient(to right, ${color1}, ${color2})`;
+    document.body.style.background = `linear-gradient(to right, ${color1}, ${color2})`;
+}
+
+// Initial gradient preview
+generateGradient();
